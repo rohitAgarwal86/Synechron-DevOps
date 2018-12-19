3acbdfe5c9b0f3596f75630106bc08ad72dd3bea
Rohit ,Agrawal <rohitAgarwal86@yahoo.com>
2018-12-19 15:58:59 +0530

    Demo For Jenkin and Git

---
+++ script-for-jenkins.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo "{INFO} First Build Demo for Jenkin and Git Integation"
+ls -ltr
+touch 
+ls -ltr
+cat demo
+echo "{INFO} Sending data to demo file" >> demo
+ls -ltr
+cat demo
+
+echo ${var1}
+
+echo "##############################################"
+echo "Printing value for server name "
+echo "##############################################"
